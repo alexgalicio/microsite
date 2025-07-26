c33522c7777777d02e7b1ef88b7ff7054977b698
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-26 22:47:08 +0800

    feat: add dashboard layout with sidebar and header

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import { AppSidebar } from "@/components/layout/app-sidebar";
+import Header from "@/components/layout/header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider defaultOpen>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
