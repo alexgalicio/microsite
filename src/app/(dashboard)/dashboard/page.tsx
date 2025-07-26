4ea512ba311e6f6972f3716fbe3ae515e627b016
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-26 22:44:18 +0800

    feat: add initial dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,3 @@
-import { SignOutButton } from "@clerk/nextjs";
-
 export default function Page() {
-  return (
-    <>
-      <h1>logged in</h1>
-      <SignOutButton />
-    </>
-  );
+  return <h1>Dashboard</h1>;
 }
