0e891444dd73a24e4645c0964e189440ecaf8b45
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-26 23:01:38 +0800

    chore: define navigation item interfaces

---
+++ src/lib/types.ts
@@ -0,0 +1,33 @@
+import { LucideIcon } from "lucide-react";
+
+export interface NavItem {
+  title: string;
+  url: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: LucideIcon;
+  label?: string;
+  description?: string;
+  isActive?: boolean;
+  items?: NavItem[];
+}
+
+export interface NavItemWithChildren extends NavItem {
+  items: NavItemWithChildren[];
+}
+
+export interface NavItemWithOptionalChildren extends NavItem {
+  items?: NavItemWithChildren[];
+}
+
+export interface FooterItem {
+  title: string;
+  items: {
+    title: string;
+    href: string;
+    external?: boolean;
+  }[];
+}
+
+export type MainNavItem = NavItemWithOptionalChildren;
+export type SidebarNavItem = NavItemWithChildren;
