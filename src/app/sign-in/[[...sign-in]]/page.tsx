1a130f39edad32452589c534c3c66e819a128a43
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-26 22:49:01 +0800

    chore: fix spacing and normalize quotes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return (
+  return (
     <div className="flex justify-center items-center min-h-screen">
       <SignIn />
     </div>
-  )
+  );
 }
