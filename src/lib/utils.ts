85d510adb522a49683c6825aa14f2ec55a1c670a
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-28 23:29:44 +0800

    feat: add error handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const handleError = (error: unknown): string => {
+  if (error instanceof Error) {
+    return error.message;
+  } else {
+    return "An error occured.";
+  }
+};
