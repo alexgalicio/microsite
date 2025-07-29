8b3ed3553c3e170129c246a127a7e8be1bcee801
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-29 19:34:54 +0800

    style: fix formatting

--- src/components/manage-menu/submenu/dialogs.tsx
+++ src/components/manage-menu/submenu/dialogs.tsx
@@ -1,40 +1,40 @@
 "use client";
 
-import { useSubmenu } from './submenu-context';
-import { SubmenuActionDialog } from './edit-dialog';
-import { SubmenuDeleteDialog } from './delete-dialog';
+import { useSubmenu } from "./submenu-context";
+import { SubmenuActionDialog } from "./edit-dialog";
+import { SubmenuDeleteDialog } from "./delete-dialog";
 
 export function SubmenuDialogs() {
-  const { open, setOpen, currentRow, setCurrentRow } = useSubmenu()
+  const { open, setOpen, currentRow, setCurrentRow } = useSubmenu();
   return (
     <>
       {currentRow && (
         <>
           <SubmenuActionDialog
             key={`submenu-edit-${currentRow.id}`}
-            open={open === 'edit'}
+            open={open === "edit"}
             onOpenChange={() => {
-              setOpen('edit')
+              setOpen("edit");
               setTimeout(() => {
-                setCurrentRow(null)
-              }, 500)
+                setCurrentRow(null);
+              }, 500);
             }}
             currentRow={currentRow}
           />
 
           <SubmenuDeleteDialog
             key={`submenu-delete-${currentRow.id}`}
-            open={open === 'delete'}
+            open={open === "delete"}
             onOpenChange={() => {
-              setOpen('delete')
+              setOpen("delete");
               setTimeout(() => {
-                setCurrentRow(null)
-              }, 500)
+                setCurrentRow(null);
+              }, 500);
             }}
             currentRow={currentRow}
           />
         </>
       )}
     </>
-  )
+  );
 }
