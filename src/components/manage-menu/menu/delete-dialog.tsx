3318d72942637d07ac1b14ec23a7a3d94a0c9109
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-29 19:13:19 +0800

    style: style menu delete dialog

--- src/components/manage-menu/menu/delete-dialog.tsx
+++ src/components/manage-menu/menu/delete-dialog.tsx
@@ -2,13 +2,14 @@
 
 import { Menu } from "@/lib/schema";
 import { useState } from "react";
-import { ConfirmDialog } from "../../confirm-dialog";
-import { Input } from "../../ui/input";
-import { Alert, AlertDescription, AlertTitle } from "../../ui/alert";
+import { ConfirmDialog } from "@/components/confirm-dialog";
+import { Input } from "@/components/ui/input";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { deleteMenu } from "@/lib/actions";
 import { toast } from "sonner";
 import { handleError } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import { OctagonAlertIcon } from "lucide-react";
 
 interface Props {
   open: boolean;
@@ -24,14 +25,20 @@ export function MenuDeleteDialog({ open, onOpenChange, currentRow }: Props) {
   async function handleDelete() {
     if (value.trim() !== currentRow.title) return;
 
+    setIsLoading(true);
     try {
-      setIsLoading(true);
-      await deleteMenu(currentRow.id);
-       toast.success("Menu deleted successfully");
+      const result = await deleteMenu(currentRow.id);
+      if (result?.error) {
+        toast.error(result.error);
+      } else {
+        toast.success("Menu deleted successfully");
+      }
+
       onOpenChange(false);
       router.refresh();
     } catch (error) {
       toast.error(handleError(error));
+      console.error("Menu Delete Dialog Error: ", error);
     } finally {
       setIsLoading(false);
     }
@@ -46,20 +53,25 @@ export function MenuDeleteDialog({ open, onOpenChange, currentRow }: Props) {
       title={`Confirm deletion of ${currentRow.title}`}
       desc={
         <div className="space-y-4">
-          <Alert variant="destructive">
-            <AlertTitle>You cannot recover this menu once deleted.</AlertTitle>
-            <AlertDescription>All menu data will be lost.</AlertDescription>
+          <Alert
+            variant="destructive"
+            className="bg-destructive/10 dark:bg-destructive/20 border-destructive/50 dark:border-destructive/70"
+          >
+            <OctagonAlertIcon className="w-4 h-4" />
+            <AlertTitle>You cannot recover this menu once deleted</AlertTitle>
+            <AlertDescription>
+              <p>
+                Your menu{" "}
+                <strong className="font-medium">{currentRow.title}</strong> and
+                all its contents will be permanently deleted.
+              </p>
+            </AlertDescription>
           </Alert>
-
-          <p>
-            Your menu {currentRow.title} and all its contents will be
-            permanently deleted.
-          </p>
 
           <div className="space-y-2">
             <p>
-              Type <span className="font-medium">{currentRow.title}</span> to
-              confirm.
+              Type &quot;<span className="font-medium">{currentRow.title}</span>
+              &quot; to confirm.
             </p>
             <Input
               value={value}
