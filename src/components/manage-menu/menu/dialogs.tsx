41d559d59b5c12afb09ec70022ba4c670eff779c
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-29 19:15:30 +0800

    style: fix formatting

--- src/components/manage-menu/menu/dialogs.tsx
+++ src/components/manage-menu/menu/dialogs.tsx
@@ -1,46 +1,46 @@
 "use client";
 
-import { useMenu } from './menu-context'
-import { MenuActionDialog } from './action-dialog'
-import { MenuDeleteDialog } from './delete-dialog'
+import { useMenu } from "./menu-context";
+import { MenuActionDialog } from "./action-dialog";
+import { MenuDeleteDialog } from "./delete-dialog";
 
 export function MenuDialogs() {
-  const { open, setOpen, currentRow, setCurrentRow } = useMenu()
+  const { open, setOpen, currentRow, setCurrentRow } = useMenu();
   return (
     <>
       <MenuActionDialog
-        key='menu-add'
-        open={open === 'add'}
-        onOpenChange={() => setOpen('add')}
+        key="menu-add"
+        open={open === "add"}
+        onOpenChange={() => setOpen("add")}
       />
 
       {currentRow && (
         <>
           <MenuActionDialog
             key={`menu-edit-${currentRow.id}`}
-            open={open === 'edit'}
+            open={open === "edit"}
             onOpenChange={() => {
-              setOpen('edit')
+              setOpen("edit");
               setTimeout(() => {
-                setCurrentRow(null)
-              }, 500)
+                setCurrentRow(null);
+              }, 500);
             }}
             currentRow={currentRow}
           />
 
           <MenuDeleteDialog
             key={`menu-delete-${currentRow.id}`}
-            open={open === 'delete'}
+            open={open === "delete"}
             onOpenChange={() => {
-              setOpen('delete')
+              setOpen("delete");
               setTimeout(() => {
-                setCurrentRow(null)
-              }, 500)
+                setCurrentRow(null);
+              }, 500);
             }}
             currentRow={currentRow}
           />
         </>
       )}
     </>
-  )
+  );
 }
