22241dc1e70f3b41ef331c77045db510fde214bf
Alex Galicio <alexisjoygalicio@gmail.com>
2025-07-30 12:34:20 +0800

    feat: add initial microsite page

--- src/app/(dashboard)/microsites/page.tsx
+++ src/app/(dashboard)/microsites/page.tsx
@@ -1,5 +1,5 @@
 import PageContainer from "@/components/layout/page-container";
-import CreateAccount from "@/components/microsites/create-account";
+import NewMicrosite from "@/components/microsites/new-site";
 
 export default async function Page() {
   return (
@@ -7,12 +7,13 @@ export default async function Page() {
       <div className="flex flex-1 flex-col gap-4">
         <div className="flex items-start justify-between">
           <h2 className="text-2xl font-bold tracking-tight">Microsites</h2>
-          <CreateAccount />
+          <NewMicrosite />
         </div>
 
         {/* <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12">
             microsite table
           </div> */}
+
       </div>
     </PageContainer>
   );
