bdcd5b457f95170a2a67615baaa1ec087c402de8
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-01 21:59:48 +0800

    refactor: uncomment site data fetching logic in editor page

--- src/app/editor/[siteId]/page.tsx
+++ src/app/editor/[siteId]/page.tsx
@@ -15,26 +15,26 @@ export default async function Page({ params }: Props) {
   const { siteId } = await params;
   console.log("Site ID:", siteId);
   
-  // const supabase = createServerSupabaseClient();
-  // const { data, error } = await supabase
-  //   .from("sites")
-  //   .select("*")
-  //   .eq("id", siteId)
-  //   .single();
+  const supabase = createServerSupabaseClient();
+  const { data, error } = await supabase
+    .from("sites")
+    .select("*")
+    .eq("id", siteId)
+    .single();
 
-  // if (error) {
-  //   console.error("Error fetching site:", error);
-  // }
+  if (error) {
+    console.error("Error fetching site:", error);
+  }
 
-  // if (!data || !(session.userId === data.user_id)) {
-  //   redirect("/sign-in");
-  // }
+  if (!data || !(session.userId === data.user_id)) {
+    redirect("/sign-in");
+  }
 
   return (
     <div>
       <>
         <h1>Editor Page</h1>
-        {/* <h1>Site data: {JSON.stringify(data)}</h1> */}
+        <h1>Site data: {JSON.stringify(data)}</h1>
       </>
     </div>
   );
