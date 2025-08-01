bda9bef2a69aefc48641fb6059dd01ebb76b4bc3
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-01 20:47:49 +0800

    feat: add ArchiveSiteDialog and RestoreSiteDialog to SiteDialogs component

--- src/components/microsites/dialogs.tsx
+++ src/components/microsites/dialogs.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { SiteActionDialog } from "./action-dialog";
+import { ArchiveSiteDialog } from "./archive-site";
+import { RestoreSiteDialog } from "./restore-dialog";
 import { useSite } from "./site-context";
 
 export function SiteDialogs() {
@@ -20,6 +22,30 @@ export function SiteDialogs() {
             open={open === "edit"}
             onOpenChange={() => {
               setOpen("edit");
+              setTimeout(() => {
+                setCurrentRow(null);
+              }, 500);
+            }}
+            currentRow={currentRow}
+          />
+
+          <ArchiveSiteDialog
+            key={`site-archive-${currentRow.id}`}
+            open={open === "archive"}
+            onOpenChange={() => {
+              setOpen("archive");
+              setTimeout(() => {
+                setCurrentRow(null);
+              }, 500);
+            }}
+            currentRow={currentRow}
+          />
+
+          <RestoreSiteDialog
+            key={`site-restore-${currentRow.id}`}
+            open={open === "restore"}
+            onOpenChange={() => {
+              setOpen("restore");
               setTimeout(() => {
                 setCurrentRow(null);
               }, 500);
