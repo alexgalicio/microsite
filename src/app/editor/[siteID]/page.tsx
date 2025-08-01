339d61d190b4f6038a89a21ea87c77f6703da482
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-01 21:23:04 +0800

    refactor: remove unnecessary whitespace in editor page

--- src/app/editor/[siteID]/page.tsx
+++ src/app/editor/[siteID]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({ params }: Props) {
   
   const { siteId } = await params;
   console.log("Site ID:", siteId);
-
+  
   // const supabase = createServerSupabaseClient();
   // const { data, error } = await supabase
   //   .from("sites")
