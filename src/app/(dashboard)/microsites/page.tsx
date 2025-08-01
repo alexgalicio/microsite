35ee6104d74f2103b6fb7b29d1ba4c59019bfd71
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-01 20:51:48 +0800

    feat: update layout and conditional rendering in microsites page

--- src/app/(dashboard)/microsites/page.tsx
+++ src/app/(dashboard)/microsites/page.tsx
@@ -1,6 +1,7 @@
 import PageContainer from "@/components/layout/page-container";
 import { SiteDialogs } from "@/components/microsites/dialogs";
 import SiteProvider from "@/components/microsites/site-context";
+import PageItem from "@/components/microsites/site-item";
 import { SitePrimaryButtons } from "@/components/microsites/site-primary-buttons";
 import SearchMicrosites from "@/components/microsites/toolbar";
 import { getAllSite, getSiteById } from "@/lib/actions";
@@ -28,11 +29,19 @@ export default async function Page() {
     <PageContainer>
       <SiteProvider>
         <div className="flex flex-1 flex-col gap-4">
-          <div className="flex items-start justify-between items-center">
+          <div className="flex items-center justify-between">
             <h2 className="text-2xl font-bold tracking-tight">Microsites</h2>
             <SitePrimaryButtons />
           </div>
-          <SearchMicrosites sites={sites} />
+          {isAdmin ? (
+            <SearchMicrosites sites={sites} />
+          ) : (
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+              {sites.map((site) => (
+                <PageItem key={site.id} site={site} />
+              ))}
+            </div>
+          )}
         </div>
         <SiteDialogs />
       </SiteProvider>
