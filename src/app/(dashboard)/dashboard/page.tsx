6f1411d2af8a98b0673c4f2a5f92e03ac2b1d892
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-02 23:43:39 +0800

    refactor: wrap CreateAccount component in PageContainer for improved layout

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import PageContainer from "@/components/layout/page-container";
 import CreateAccount from "@/components/microsites/create-account";
 
 export default function Page() {
-  return <CreateAccount />;
+  return (
+    <PageContainer>
+      <CreateAccount />
+    </PageContainer>
+  );
 }
