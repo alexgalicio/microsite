8a71e2756d80aa24e0d5ed99d191da8f600b5c4b
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-03 02:47:50 +0800

    refactor: move server actions on menu.ts

--- src/app/(dashboard)/manage-menu/page.tsx
+++ src/app/(dashboard)/manage-menu/page.tsx
@@ -1,42 +1,19 @@
 import PageContainer from "@/components/layout/page-container";
-import { columns } from "@/components/manage-menu/menu/columns";
 import MenuProvider from "@/components/manage-menu/menu/menu-context";
+import { columns } from "@/components/manage-menu/menu/columns";
 import { MenuDialogs } from "@/components/manage-menu/menu/dialogs";
-import { MenuPrimaryButtons } from "@/components/manage-menu/menu/primary-button";
+import { CreateMenuButton } from "@/components/manage-menu/menu/create-menu";
 import { DataTable } from "@/components/manage-menu/data-table";
-import { Menu } from "@/lib/schema";
-import { createServerSupabaseClient } from "@/utils/server";
-
-async function getData(): Promise<{ data: Menu[]; error: string | null }> {
-  const supabase = createServerSupabaseClient();
-  const { data, error } = await supabase.from("menu").select(`
-        id, 
-        title,
-        submenus:submenu(count)
-      `);
-
-  if (error) return { data: [], error: error.message };
-
-  const mappedData =
-    data?.map((menu) => ({
-      id: menu.id,
-      title: menu.title,
-      submenu_count: menu.submenus?.[0]?.count || 0,
-    })) || [];
-
-  return { data: mappedData, error: null };
-}
+import { getTableData } from "@/lib/actions/menu";
 
 export default async function Page() {
-  const { data, error } = await getData();
+  const { data, error } = await getTableData();
 
   if (error) {
     return (
       <div className="p-4">
-        <h2 className="text-xl font-semibold text-red-600">
-          Error Loading Menu Table
-        </h2>
-        <p className="text-gray-600">{error}</p>
+        <h2>Error Loading Menu Table</h2>
+        <p>{error}</p>
       </div>
     );
   }
@@ -47,7 +24,7 @@ export default async function Page() {
         <div className="flex flex-1 flex-col gap-4">
           <div className="flex items-center justify-between">
             <h2 className="text-2xl font-bold tracking-tight">Menu List</h2>
-            <MenuPrimaryButtons />
+            <CreateMenuButton />
           </div>
           <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12">
             <DataTable data={data} columns={columns} />
--- src/app/(dashboard)/manage-menu/submenu/[menuId]/page.tsx
+++ src/app/(dashboard)/manage-menu/submenu/[menuId]/page.tsx
@@ -1,33 +1,11 @@
 import PageContainer from "@/components/layout/page-container";
+import SubmenuProvider from "@/components/manage-menu/submenu/submenu-context";
+import CreateAccount from "@/components/microsites/create-account";
 import { DataTable } from "@/components/manage-menu/data-table";
 import { columns } from "@/components/manage-menu/submenu/columns";
-import SubmenuProvider from "@/components/manage-menu/submenu/submenu-context";
 import { SubmenuDialogs } from "@/components/manage-menu/submenu/dialogs";
-import CreateAccount from "@/components/microsites/create-account";
-import { Submenu } from "@/lib/schema";
-import { createServerSupabaseClient } from "@/utils/server";
-import { getMenuTitle } from "@/lib/actions";
-
-async function getData(
-  menuId: string
-): Promise<{ data: Submenu[]; error: string | null }> {
-  const supabase = createServerSupabaseClient();
-  const { data, error } = await supabase
-    .from("submenu")
-    .select(
-      `
-      id,
-      title,
-      menu_id,
-      profiles:user_id(email)
-    `
-    )
-    .eq("menu_id", menuId);
-
-  if (error) return { data: [], error: error.message };
-
-  return { data: data, error: null };
-}
+import { getMenuTitle } from "@/lib/actions/menu";
+import { getTableData } from "@/lib/actions/submenu";
 
 export default async function Page({
   params,
@@ -35,16 +13,14 @@ export default async function Page({
   params: Promise<{ menuId: string }>;
 }) {
   const { menuId } = await params;
-  const { data, error } = await getData(menuId);
-  const result = await getMenuTitle(menuId);
+  const { data, error } = await getTableData(menuId);
+  const menuTitle = await getMenuTitle(menuId);
 
-  if (error || result.error) {
+  if (error || menuTitle.error) {
     return (
       <div className="p-4">
-        <h2 className="text-xl font-semibold text-red-600">
-          Error Loading Submenu Table
-        </h2>
-        <p className="text-gray-600">{error || result.error}</p>
+        <h2>Error Loading Submenu Table</h2>
+        <p>{error || menuTitle.error}</p>
       </div>
     );
   }
@@ -54,7 +30,9 @@ export default async function Page({
       <SubmenuProvider>
         <div className="flex flex-1 flex-col gap-4">
           <div className="flex items-center justify-between">
-            <h2 className="text-2xl font-bold tracking-tight">{result.data}</h2>
+            <h2 className="text-2xl font-bold tracking-tight">
+              {menuTitle.data}
+            </h2>
             <CreateAccount />
           </div>
           <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12">
