8229eb9aedeca23270423b67948480076f6f4daa
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-03 02:23:58 +0800

    refactor: update site data fetching logic and improve metadata generation for subdomain pages

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,18 +1,48 @@
-import { getSiteBySubdomain } from "@/lib/actions";
+import { getSiteBySubdomain } from "@/lib/actions/site";
+import { Metadata } from "next";
 
-export default async function Page({
+export async function generateMetadata({
   params,
 }: {
   params: Promise<{ domain: string }>;
-}) {
+}): Promise<Metadata> {
   const { domain } = await params;
+  const result = await getSiteBySubdomain(domain);
 
-  const result = await getSiteBySubdomain(domain.toString());
-
-  // Handle error case
-  if (result.error || !result.data) {
-    return <div>Error: {result.error || "Site not found"}</div>;
+  if (!result || result.error) {
+    return {
+      title: "Microsite",
+    };
   }
 
-  return `Site name ${result.data[0]?.title}`;
+  return {
+    title: `${result.title}`,
+    description: `Subdomain page for ${domain}`,
+  };
+}
+
+export default async function SubdomainPage({
+  params,
+}: Readonly<{
+  params: Promise<{ domain: string }>;
+}>) {
+  const { domain } = await params;
+  const result = await getSiteBySubdomain(domain);
+
+  if (!result || result.error) {
+    console.error("not found")
+  }
+
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          Welcome to {result ? result.title : "Microsite"}
+        </h1>
+        <p className="mt-3 text-lg text-gray-600">
+          This is your custom subdomain page
+        </p>
+      </div>
+    </div>
+  );
 }
