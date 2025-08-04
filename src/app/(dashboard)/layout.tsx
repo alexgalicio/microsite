aceeb3f65dd615fbd95fd442a053b8501839e0e4
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-04 17:54:40 +0800

    feat(layout): wrap Header and Main in div for responsive layout adjustments

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import Header from "@/components/layout/header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Header } from "@/components/layout/header";
+import { Main } from "@/components/layout/main";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
 
 export default async function DashboardLayout({
   children,
@@ -10,10 +12,20 @@ export default async function DashboardLayout({
   return (
     <SidebarProvider defaultOpen>
       <AppSidebar />
-      <SidebarInset>
+      <div
+        className={cn(
+          "ml-auto w-full max-w-full",
+          "peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon))]",
+          "peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]",
+          "sm:transition-[width] sm:duration-200 sm:ease-linear",
+          "flex h-svh flex-col",
+          "group-data-[scroll-locked=1]/body:h-full",
+          "has-[main.fixed-main]:group-data-[scroll-locked=1]/body:h-svh"
+        )}
+      >
         <Header />
-        {children}
-      </SidebarInset>
+        <Main>{children}</Main>
+      </div>
     </SidebarProvider>
   );
 }
