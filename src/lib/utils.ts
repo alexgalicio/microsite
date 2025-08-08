22a33cb1593208629a55be1e5eaee4416428255f
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-08 23:06:05 +0800

    feat: add formate date function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,3 +13,7 @@ export const handleError = (error: unknown): string => {
     return "An error occured.";
   }
 };
+
+export function formatDate(dateString : string) {
+  return format(new Date(dateString), 'MMMM d, yyyy');
+}
