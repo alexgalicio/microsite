119b0d06e724163cad6138fa92f11abc82acf165
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-09 17:17:30 +0800

    feat: add background image

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,14 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div
+      className="flex justify-center items-center min-h-screen"
+      style={{
+        backgroundImage: 'url("/images/background.png")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <SignIn />
     </div>
   );
