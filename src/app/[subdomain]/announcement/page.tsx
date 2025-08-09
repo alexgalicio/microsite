b0a7c56b4afa1330c0b830cc4d2267a07d276c5e
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-09 10:44:09 +0800

    feat: add pagination

--- src/app/[subdomain]/announcement/page.tsx
+++ src/app/[subdomain]/announcement/page.tsx
@@ -1,36 +1,59 @@
 import AnnouncementsPreview from "@/components/announcement/announcement-preview";
 import Header from "@/components/announcement/header";
+import { PaginationWithLinks } from "@/components/announcement/pagination";
 import { getSiteBySubdomain } from "@/lib/actions/site";
 import { createServerSupabaseClient } from "@/utils/server";
 
 export default async function Page({
   params,
+  searchParams,
 }: {
-  params: Promise<{ subdomain: string }>;
+  params: { subdomain: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const { subdomain } = await params;
+  const { subdomain } = params;
+  const page = parseInt(searchParams?.page as string || "1");
+  const pageSize = parseInt(searchParams?.pageSize as string || "2");
 
   const supabase = createServerSupabaseClient();
   const response = await getSiteBySubdomain(subdomain);
+  
   if (!response || !response.success) {
     return <div>Site not found</div>;
   }
 
-  // Fetch announcement for that site
-  const { data, error} = await supabase
+  // Calculate the range of records to fetch
+  const from = (page - 1) * pageSize;
+  const to = from + pageSize - 1;
+
+  // Fetch announcements with pagination
+  const { data, error, count } = await supabase
     .from("announcements")
-    .select("*")
+    .select("*", { count: 'exact' })
     .eq("site_id", response.data?.id)
-    .order("created_at", { ascending: false });
+    .order("created_at", { ascending: false })
+    .range(from, to);
 
   if (error) {
-    return <div>Failed to fetch announcement</div>;
+    return <div>Failed to fetch announcements</div>;
   }
+
+  console.log('params', params, 'search', searchParams)
+  console.log('from', from, 'to', to)
+  console.log('page', page, 'pagesise', pageSize)
 
   return (
     <div className="max-w-6xl mx-auto px-5 mb-10">
       <Header />
-      <AnnouncementsPreview announcements={data} />
+      <AnnouncementsPreview announcements={data || []} />
+      <div className="mt-8">
+        <PaginationWithLinks
+          page={page}
+          pageSize={pageSize}
+          totalCount={count || 0}
+          navigationMode="router"
+        />
+      </div>
     </div>
   );
 }
