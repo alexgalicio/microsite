0eedf471c6f74600b6a2a6ca8ba191a756badb86
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-17 18:40:43 +0800

    feat: add notification page

---
+++ src/app/(dashboard)/notifications/page.tsx
@@ -0,0 +1,17 @@
+import { Mail } from "@/components/mail/mail";
+import { getAllNotifications } from "@/lib/actions/contact-form";
+
+export default async function Page() {
+  const { data, error } = await getAllNotifications();
+
+  if (error) {
+    return (
+      <div className="p-4">
+        <h2>Error Loading Notifications Page</h2>
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  return <Mail mails={data} />;
+}
