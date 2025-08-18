101ed7c13fa9507e2198b12abcf79cc2e34c62f2
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-18 10:41:44 +0800

    refactor: removed pointer events auto

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -29,7 +29,6 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn(
-        "pointer-events-auto",
         "bg-background group/calendar p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
         String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
         String.raw`rtl:**:[.rdp-button\_previous>svg]:rotate-180`,
