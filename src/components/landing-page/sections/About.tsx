1ad429f2188b62f28830c2c55f186c8c56394ec7
Eddhan Tan <eddhan12345@gmail.com>
2025-08-20 18:07:18 +0800

    edit about and program section styles

--- src/components/landing-page/sections/About.tsx
+++ src/components/landing-page/sections/About.tsx
@@ -15,7 +15,7 @@ function About() {
                         The College of Information and Communications Technology (CICT) equips students with the knowledge and skills needed to thrive in the ever-evolving world of technology. Through innovative learning, industry-aligned programs, and hands-on experience, we prepare future IT professionals to lead, create, and innovate in the digital age.
                     </p>
                 </div>
-                <Image src={"/images/CICTFaculty.png"} alt="college faculty" width={500} height={300} className="w-full h-96 md:w-1/2 md:h-[500px] object-contain" />
+                <Image src={"/images/CICTFaculty.png"} alt="college faculty" width={500} height={300} className="w-full h-96 md:w-1/2 md:h-[500px] object-contain lg:object-cover" />
             </div>
         </section>
     )
