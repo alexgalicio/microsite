4f619572040ee71409585c94579fd6d523665b4b
Alex Galicio <alexisjoygalicio@gmail.com>
2025-08-27 17:43:28 +0800

    fix: error

--- src/app/programs/[program]/page.tsx
+++ src/app/programs/[program]/page.tsx
@@ -1,32 +1,36 @@
-import Navbar from "@/components/program-sites/sections/Navbar"
-import Hero from "@/components/program-sites/sections/Hero"
-import About from "@/components/program-sites/sections/About"
-import Subjects from "@/components/program-sites/sections/Subjects"
-import Careers from "@/components/program-sites/sections/Careers"
-import Footer from "@/components/program-sites/sections/Footer"
+import Navbar from "@/components/program-sites/sections/Navbar";
+import Hero from "@/components/program-sites/sections/Hero";
+import About from "@/components/program-sites/sections/About";
+import Subjects from "@/components/program-sites/sections/Subjects";
+import Careers from "@/components/program-sites/sections/Careers";
+import Footer from "@/components/program-sites/sections/Footer";
 
-type Prop = {
-  params : {
-    program: string
-  }
-}
+async function page({ params }: { params: Promise<{ program: string }> }) {
+  const { program } = await params;
 
-async function page({params} : Prop) {
-  const {program} = await params
-  const data = await import(`@/data/${program}.json`).then(mod => mod.default)
+  const data = await import(`@/data/${program}.json`).then(
+    (mod) => mod.default
+  );
 
   return (
     <>
-        <Navbar programName={data.acronym}></Navbar>
-        <main>
-            <Hero heading={data.heroSection.heading} subheading={data.heroSection.subheading}/>
-            <About programName = {data.acronym} heading={data.aboutSection.heading} subheading={data.aboutSection.subheading}/>
-            <Subjects years={data.subjectsSection}/>
-            <Careers careers={data.careerSection}/>
-        </main>
-        <Footer program={data.programName}/>
+      <Navbar programName={data.acronym}></Navbar>
+      <main>
+        <Hero
+          heading={data.heroSection.heading}
+          subheading={data.heroSection.subheading}
+        />
+        <About
+          programName={data.acronym}
+          heading={data.aboutSection.heading}
+          subheading={data.aboutSection.subheading}
+        />
+        <Subjects years={data.subjectsSection} />
+        <Careers careers={data.careerSection} />
+      </main>
+      <Footer program={data.programName} />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
