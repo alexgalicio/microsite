90fee80339a5f4d7845769a512aa4001f7e619f9
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-02 15:19:02 +0800

    feat: add header with mega-menu

---
+++ src/components/landing-page/navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link";
+import MegaMenu from "./mega-menu";
+
+export default function Navbar() {
+  return (
+    <div className="flex gap-6 font-normal flex-col lg:flex-row lg:items-center">
+      <Link href="#home" className="hover:text-primary px-4 py-2">
+        Home
+      </Link>
+      <Link href="#about" className="hover:text-primary px-4 py-2">
+        About
+      </Link>
+      <MegaMenu />
+    </div>
+  );
+}
