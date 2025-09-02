ab25983c0fd4057c31af2851b10359c12451f90c
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-02 15:09:42 +0800

    refactor: change ui component

--- src/app/programs/[program]/page.tsx
+++ src/app/programs/[program]/page.tsx
@@ -1,11 +1,15 @@
-import Navbar from "@/components/program-sites/sections/Navbar";
-import Hero from "@/components/program-sites/sections/Hero";
-import About from "@/components/program-sites/sections/About";
-import Subjects from "@/components/program-sites/sections/Subjects";
-import Careers from "@/components/program-sites/sections/Careers";
-import Footer from "@/components/program-sites/sections/Footer";
+import About from "@/components/landing-page/about";
+import Career from "@/components/landing-page/career";
+import Footer from "@/components/landing-page/footer";
+import Header from "@/components/landing-page/header";
+import Hero from "@/components/landing-page/hero";
+import Subject from "@/components/landing-page/subject";
 
-async function page({ params }: { params: Promise<{ program: string }> }) {
+export default async function ProgramPage({
+  params,
+}: {
+  params: Promise<{ program: string }>;
+}) {
   const { program } = await params;
 
   const data = await import(`@/data/${program}.json`).then(
@@ -14,23 +18,19 @@ async function page({ params }: { params: Promise<{ program: string }> }) {
 
   return (
     <>
-      <Navbar programName={data.acronym}></Navbar>
-      <main>
-        <Hero
-          heading={data.heroSection.heading}
-          subheading={data.heroSection.subheading}
-        />
-        <About
-          programName={data.acronym}
-          heading={data.aboutSection.heading}
-          subheading={data.aboutSection.subheading}
-        />
-        <Subjects years={data.subjectsSection} />
-        <Careers careers={data.careerSection} />
-      </main>
-      <Footer program={data.programName} />
+      <Header />
+      <Hero
+        heading={data.heroSection.heading}
+        subheading={data.heroSection.subheading}
+      />
+      <About
+        span={data.acronym}
+        title={data.aboutSection.heading}
+        description={data.aboutSection.subheading}
+      />
+      <Subject years={data.subjectsSection} />
+      <Career careers={data.careerSection} />
+      <Footer />
     </>
   );
 }
-
-export default page;
