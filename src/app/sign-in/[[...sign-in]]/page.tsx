cd5ace2ae358ba4d8d870c0db4351180c1d465ad
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-04 17:30:04 +0800

    refactor: change hero and sign in background

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,14 +3,18 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div
-      className="flex justify-center items-center min-h-screen"
+      className="relative flex justify-center items-center min-h-screen"
       style={{
-        backgroundImage: 'url("/images/background.png")',
+        backgroundImage: 'url("/images/pimentel.jpg")',
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <SignIn />
+      <div className="absolute inset-0 bg-black/20"></div>
+
+      <div className="relative">
+        <SignIn />
+      </div>
     </div>
   );
 }
--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -9,23 +9,27 @@ export default function Hero({ heading, subheading }: HeroProps) {
   return (
     <section
       id="home"
-      className="relative z-10 overflow-hidden pb-16 pb-[200px] pt-[270px]"
+      className="relative z-10 overflow-hidden pb-16 pb-[200px] pt-[280px]"
     >
       <Image
-        src="/images/background.png"
+        src="/images/pimentel.jpg"
         alt="Background"
         fill
         className="object-cover object-center -z-10"
         priority
       />
-      <div className="container mx-auto">
+      
+      {/* dark overlay */}
+      <div className="absolute inset-0 bg-black/10 -z-5"></div>
+      
+      <div className="container mx-auto relative z-10">
         <div className="flex flex-wrap">
           <div className="w-full px-4">
             <div className="mx-auto max-w-[800px] text-center">
               <h1 className="mb-5 text-3xl font-bold leading-tight text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                 {heading}
               </h1>
-              <p className="text-base leading-relaxed! text-white sm:text-lg md:text-xl">
+              <p className="text-base leading-relaxed text-white sm:text-lg md:text-xl">
                 {subheading}
               </p>
             </div>
