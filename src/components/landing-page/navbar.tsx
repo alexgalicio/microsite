2c0772e6da5247662d18b8375a95762f7f934074
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-08 18:48:45 +0800

    feat: add programs and contact on header

--- src/components/landing-page/navbar.tsx
+++ src/components/landing-page/navbar.tsx
@@ -6,11 +6,17 @@ import MegaMenu from "./mega-menu";
 export default function Navbar() {
   return (
     <div className="flex gap-6 font-normal flex-col lg:flex-row lg:items-center">
-      <Link href="#home" className="hover:text-primary px-4 py-2">
+      <Link href="/" className="hover:text-primary px-4 py-2">
         Home
+      </Link>
+      <Link href="/#programs" className="hover:text-primary px-4 py-2">
+        Programs
       </Link>
       <Link href="#about" className="hover:text-primary px-4 py-2">
         About
+      </Link>
+      <Link href="/#contact" className="hover:text-primary px-4 py-2">
+        Contact
       </Link>
       <MegaMenu />
     </div>
