17d7c52a93d317c7188d5aef2d414145e26745c8
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-08 19:12:02 +0800

    feat: add date created and badges

--- src/components/links/link-preview.tsx
+++ src/components/links/link-preview.tsx
@@ -1,21 +1,44 @@
+import Link from "next/link";
 import { Links } from "@/lib/types";
 import { ExternalLink } from "lucide-react";
-import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { format } from "date-fns";
 
 export default function LinkPreview({ link }: { link: Links }) {
   return (
-    <div className="rounded border-l-4 border-l-muted-foreground border hover:shadow-md p-4 transition">
-      <h2 className="font-semibold">
-        <Link href={link.url} target="_blank" rel="noopener noreferrer">
+    <div className="rounded-lg border overflow-hidden hover:shadow-md transition-all p-4 bg-background">
+      <div className="mb-3">
+        <Link
+          href={link.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex-1"
+        >
           <div className="flex items-center gap-2 group w-fit">
-            <h2 className="text-xl font-semibold transition-colors hover:underline">
+            <h2 className="text-lg font-semibold transition-colors group-hover:underline">
               {link.title}
             </h2>
             <ExternalLink className="w-4 h-4 opacity-0 transition-opacity group-hover:opacity-60" />
           </div>
         </Link>
-      </h2>
-      <p className="mt-2 text-sm">{link.description}</p>
+      </div>
+
+      <p className="line-clamp-2 text-gray-500 text-sm mb-4">
+        {link.description}
+      </p>
+
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
+        <p className="text-gray-500 text-xs sm:text-sm order-2 sm:order-1">
+          {format(new Date(link.created_at), "M/d/yyyy")}
+        </p>
+
+        <div className="flex flex-wrap gap-1 order-1 sm:order-2">
+          <Badge className="bg-green-500 text-xs">
+            {link.link_category.title}
+          </Badge>
+          <Badge className="bg-purple-500 text-xs">{link.link_to.title}</Badge>
+        </div>
+      </div>
     </div>
   );
 }
