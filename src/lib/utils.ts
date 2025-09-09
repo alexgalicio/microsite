b3e719e28d1233ce84490778eaa105f7a3cf3dce
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-09 18:36:49 +0800

    refactor: remove formatDate

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { format } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,7 +12,3 @@ export const handleError = (error: unknown): string => {
     return "An error occured.";
   }
 };
-
-export function formatDate(dateString : string) {
-  return format(new Date(dateString), 'MMMM d, yyyy');
-}
