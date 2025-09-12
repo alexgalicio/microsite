c6cd0e5e1febb4471558c5b8d3a5f51bbec3ff22
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-12 10:38:46 +0800

    feat: add facebook feed item custom title

--- src/hooks/use-breadcrumbs.ts
+++ src/hooks/use-breadcrumbs.ts
@@ -11,6 +11,7 @@ type BreadcrumbItem = {
 // This allows to add custom title as well
 const routeMapping: Record<string, BreadcrumbItem[]> = {
   "/manage-menu": [{ title: "Manage Menu", link: "/manage-menu" }],
+  "/facebook-feed": [{ title: "Facebook Feed", link: "/facebook-feed" }],
 };
 
 export function useBreadcrumbs() {
