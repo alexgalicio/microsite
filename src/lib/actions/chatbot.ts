2b60e6f4ebfe3cd7b54f49dd63fe6c924efc013d
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-19 18:54:11 +0800

    feat: add actions to get chunks data and delete files

--- src/lib/actions/chatbot.ts
+++ src/lib/actions/chatbot.ts
@@ -2,6 +2,7 @@
 
 import { createServerSupabaseClient } from "@/utils/server";
 import { FeedbackType } from "../types";
+import { Chunks } from "../schema";
 
 export async function saveUserFeedback(type: FeedbackType) {
   const supabase = createServerSupabaseClient();
@@ -26,6 +27,62 @@ export async function saveUserQuestion(question: string) {
 
   if (error) {
     return { success: false, error: error.message };
+  }
+
+  return { success: true };
+}
+
+export async function getTableData(): Promise<{
+  data: Chunks[];
+  error: string | null;
+}> {
+  const supabase = createServerSupabaseClient();
+  const { data, error } = await supabase.from("chunks").select(`
+        id, 
+        filename,
+        url
+      `);
+
+  if (error) return { data: [], error: error.message };
+
+  const mappedData =
+    data?.map((chunks) => ({
+      id: chunks.id,
+      filename: chunks.filename,
+      url: chunks.url,
+    })) || [];
+
+  // remove duplicates by filename
+  const uniqueData = Array.from(
+    new Map(mappedData.map((item) => [item.filename, item])).values()
+  );
+
+  return { data: uniqueData, error: null };
+}
+
+export async function deleteFile(filename: string) {
+  const supabase = createServerSupabaseClient();
+
+  // delete file from storage
+  const { error: storageError } = await supabase.storage
+    .from("media")
+    .remove([filename]);
+
+  if (storageError) {
+    return {
+      success: false,
+      error: storageError.message,
+    };
+  }
+
+  // delete from chunks table
+  const { error: dbError } = await supabase
+    .from("chunks")
+    .delete()
+    .eq("filename", filename);
+
+  if (dbError) {
+    return { success: false, error: dbError.message };
   }
 
   return { success: true };
