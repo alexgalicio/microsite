7d4f0cb2038da9f1d0bb62868f1e0fd817bebfdc
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-19 18:59:20 +0800

    refactor: edit prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
-import { embed, streamText } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
+import { embed, streamText } from "ai";
+import { type NextRequest } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 const openai = createOpenAI({
@@ -22,7 +23,7 @@ async function fetchRelevantContext(embedding: number[]) {
   const { data, error } = await supabase.rpc("get_relevant_chunks", {
     query_vector: embedding,
     match_threshold: 0.5,
-    match_count: 10,
+    match_count: 3,
   });
 
   console.log("data", data);
@@ -40,13 +41,20 @@ async function fetchRelevantContext(embedding: number[]) {
   );
 }
 
-// system message template
 function createPrompt(context: string, userQuestion: string) {
   return {
     role: "system",
     content: `
-      You are Foxy, a helpful assistant for College of Information and Communications Technology (CICT) in Bulacan State University.
-      Use the following context to answer questions: 
+      You are Foxy, a friendly and knowledgeable assistant for the College
+      of Information and Communitcations Technology.
+      
+      Your responses should be helpful and accurate, drawing from the context provided.
+      Always include relevant URLs from the context when available.
+
+      Use the provided context as your primary source. 
+      If you find related information that might answer the question, mention it.
+      Only say you don't know if there's truly no relevant information in the context.
+      Don't repeat yourself unnecessarily.
       ----------------
       START CONTEXT
       ${context}
@@ -54,8 +62,6 @@ function createPrompt(context: string, userQuestion: string) {
       ----------------
       
       Return the answer in markdown format including relevant links.
-      Where the above context does not provide enough information relating to the question provide an answer based on your own knowledge but caveat it so the user
-      knows that it may not be up to date.
       
       ----------------
       QUESTION: ${userQuestion}
@@ -63,7 +69,7 @@ function createPrompt(context: string, userQuestion: string) {
   };
 }
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const { messages } = await req.json();
     const latestMessage = messages.at(-1).content;
