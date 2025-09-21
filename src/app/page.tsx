31df6b378f08bc8b5089ff406203d4ee30be3076
Alex Galicio <alexisjoygalicio@gmail.com>
2025-09-21 16:33:00 +0800

    refactor: remove unsused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { ArrowDownCircleIcon, MessageCircle, X } from "lucide-react";
+import { X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   Card,
