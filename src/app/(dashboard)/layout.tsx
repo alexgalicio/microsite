8dd0d4ee6e615ef371f6914ec22410e1dbfa195a
Alex Galicio <alexisjoygalicio@gmail.com>
2025-10-05 17:20:33 +0800

    refactor: fix ui

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
 import { Header } from "@/components/layout/header";
 import { Main } from "@/components/layout/main";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 
 export default async function DashboardLayout({
@@ -12,20 +12,17 @@ export default async function DashboardLayout({
   return (
     <SidebarProvider defaultOpen>
       <AppSidebar />
-      <div
-        className={cn(
-          "ml-auto w-full max-w-full",
-          "peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon))]",
-          "peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]",
-          "sm:transition-[width] sm:duration-200 sm:ease-linear",
-          "flex h-svh flex-col",
-          "group-data-[scroll-locked=1]/body:h-full",
-          "has-[main.fixed-main]:group-data-[scroll-locked=1]/body:h-svh"
-        )}
-      >
-        <Header />
-        <Main>{children}</Main>
-      </div>
+      <SidebarInset>
+        <div
+          className={cn(
+            'has-[[data-layout=fixed]]:h-svh',
+            "peer-data-[variant=inset]:has-[[data-layout=fixed]]:h-[calc(100svh-(var(--spacing)*4))]"
+          )}
+        >
+          <Header />
+          <Main>{children}</Main>
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
