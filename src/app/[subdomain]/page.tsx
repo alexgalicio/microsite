4b13818d18ec07506311227a69cf9ae311d84b62
Alex Galicio <alexisjoygalicio@gmail.com>
2025-10-05 17:21:08 +0800

    feat: add loading state

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -5,9 +5,11 @@ import { getSiteData } from "@/lib/actions/site";
 import { useSite } from "@/components/subdomain-provider";
 import { useEffect, useState } from "react";
 import { handleError } from "@/lib/utils";
+import Image from "next/image";
 
 export default function SubdomainPage() {
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
   const [showPrivateSite, setShowPrivateSite] = useState(false);
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
@@ -16,20 +18,59 @@ export default function SubdomainPage() {
   useEffect(() => {
     const fetchData = async () => {
       setError(null);
+      setLoading(true);
 
-      const response = await getSiteData(site.id);
-      if (response.success && response.data) {
-        setHtml(response.data.html || "");
-        setCss(response.data.css || "");
-        setShowPrivateSite(false);
-      } else if (!response || !response.data) {
-        setShowPrivateSite(true);
-      } else {
-        setError(handleError(response.error));
+      try {
+        const response = await getSiteData(site.id);
+        if (response.success && response.data) {
+          setHtml(response.data.html || "");
+          setCss(response.data.css || "");
+          setShowPrivateSite(false);
+        } else if (!response || !response.data) {
+          setShowPrivateSite(true);
+        } else {
+          setError(handleError(response.error));
+        }
+      } catch (error) {
+        setError(handleError(error));
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
   }, [site.id]);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col min-h-screen items-center justify-center text-gray-500 px-4">
+        <div className="flex flex-row gap-4 mb-6 items-center">
+          <Image
+            src="/images/bulsu.png"
+            alt="Bulsu Logo"
+            width={100}
+            height={100}
+            className="object-contain"
+          />
+
+          <Image
+            src="/images/cict.png"
+            alt="CICT Logo"
+            width={100}
+            height={100}
+            className="object-contain"
+          />
+        </div>
+
+        <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-1 bg-gray-200 rounded overflow-hidden">
+          <div className="h-full bg-primary animate-progress"></div>
+        </div>
+
+        <p className="mt-4 text-gray-600 text-center text-sm sm:text-base">
+          Loading...
+        </p>
+      </div>
+    );
+  }
 
   if (error) {
     return (
