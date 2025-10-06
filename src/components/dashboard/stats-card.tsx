8a4ed2610561a8efce5765ecaa6593afe13b44a8
Alex Galicio <alexisjoygalicio@gmail.com>
2025-10-06 11:16:21 +0800

    feat: add tooltip, description, and change design

--- src/components/dashboard/stats-card.tsx
+++ src/components/dashboard/stats-card.tsx
@@ -1,14 +1,22 @@
 import {
   Card,
   CardAction,
-  CardDescription,
+  CardContent,
   CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { CircleMinus, Info, TrendingDown, TrendingUp } from "lucide-react";
 
 interface StatsCardProps {
   title: string;
   total: number;
   difference: number;
+  description: string;
   icon: React.ReactElement;
 }
 
@@ -16,30 +24,58 @@ export default function StatsCard({
   title,
   total,
   difference,
+  description,
   icon,
 }: StatsCardProps) {
+  const isPositive = difference > 0;
+  const isNegative = difference < 0;
+
+  const diffColor = isPositive
+    ? "text-emerald-500"
+    : isNegative
+    ? "text-red-500"
+    : "text-gray-500";
+
+  const ArrowIcon = isPositive
+    ? TrendingUp
+    : isNegative
+    ? TrendingDown
+    : CircleMinus;
+
   return (
-    <Card className="shadow-none">
+    <Card>
       <CardHeader>
-        <CardDescription>{title}</CardDescription>
-        <div className="flex flex-col gap-2">
-          <h4 className="text-2xl lg:text-3xl font-bold">{total}</h4>
-          <div className="text-muted-foreground text-sm">
-            <span
-              className={difference >= 0 ? "text-green-600" : "text-red-600"}
-            >
-              {difference >= 0 ? `+${difference}` : difference}
-            </span>{" "}
-            from last month
-          </div>
-        </div>
-        <CardAction>
-          <div className="flex gap-4">
-            <div className="bg-muted flex size-12 items-center justify-center rounded-full border">
+        <CardTitle>
+          <div className="flex flex-row items-center justify-between gap-5">
+            <div className="tracking-tight flex items-center gap-2 truncate text-sm font-medium">
               {icon}
+              {title}
             </div>
           </div>
+        </CardTitle>
+        <CardAction>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Info size={18} />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{description}</p>
+            </TooltipContent>
+          </Tooltip>
         </CardAction>
+
+        <CardContent className="p-0">
+          <div className="text-2xl font-bold">
+            <span>{total}</span>
+          </div>
+          <div className={`flex items-center gap-2 font-medium ${diffColor}`}>
+            <p className="text-sm">
+              {difference > 0 ? `+${difference}` : difference}
+            </p>
+            {ArrowIcon && <ArrowIcon size={18} />}
+          </div>
+          <p className="text-sm tracking-tight">vs Previous 30 Days</p>
+        </CardContent>
       </CardHeader>
     </Card>
   );
