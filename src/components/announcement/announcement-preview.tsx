3489b14146ac861ab1303c9895722322187ad189
Alex Galicio <alexisjoygalicio@gmail.com>
2025-10-13 19:29:16 +0800

    style: improve ui

--- src/components/announcement/announcement-preview.tsx
+++ src/components/announcement/announcement-preview.tsx
@@ -18,43 +18,44 @@ export default function AnnouncementPreview({
         Announcements
       </h2>
 
-      <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+      <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
         {announcements.map((announcement) => (
-          <Link
-            href={`/announcements/${announcement.slug}`}
+          <div
             key={announcement.id}
-            className="group"
+            className="bg-muted overflow-hidden rounded-lg"
           >
-            <AspectRatio
-              ratio={16 / 9}
-              className="w-full overflow-hidden rounded-lg"
+            <Link
+              href={`/announcements/${announcement.slug}`}
+              className="group"
             >
-              <Image
-                src={announcement.cover || "/images/placeholder.webp"}
-                alt={announcement.title}
-                fill
-                sizes="100%"
-                priority
-                className="h-full min-h-full min-w-full object-cover object-center transition-transform duration-400 ease-in-out group-hover:scale-105"
-              />
-            </AspectRatio>
+              <AspectRatio ratio={16 / 9} className="w-full">
+                <Image
+                  src={announcement.cover || "/images/placeholder.webp"}
+                  alt={announcement.title}
+                  fill
+                  sizes="100%"
+                  priority
+                  className="h-full min-h-full min-w-full object-cover object-center transition-transform duration-400 ease-in-out group-hover:scale-105"
+                />
+              </AspectRatio>
 
-            <div className="flex flex-col gap-1 pt-2">
-              <p className="text-sm text-muted-foreground ">
-                {format(new Date(announcement.created_at), "MMMM d, yyyy")}
-              </p>
+              <div className="flex flex-col gap-2 p-4">
+                <p className="text-sm text-muted-foreground ">
+                  {format(new Date(announcement.created_at), "MMMM d, yyyy")}
+                </p>
 
-              <h3 className="font-semibold tracking-tighter text-xl line-clamp-2 md:text-lg">
-                {announcement.title}
-              </h3>
+                <h3 className="font-semibold tracking-tighter text-xl line-clamp-2 md:text-lg">
+                  {announcement.title}
+                </h3>
 
-              <p className="line-clamp-2">
-                {DOMPurify.sanitize(announcement.content, {
-                  ALLOWED_TAGS: [],
-                })}
-              </p>
-            </div>
-          </Link>
+                <p className="line-clamp-2">
+                  {DOMPurify.sanitize(announcement.content, {
+                    ALLOWED_TAGS: [],
+                  })}
+                </p>
+              </div>
+            </Link>
+          </div>
         ))}
       </div>
     </>
