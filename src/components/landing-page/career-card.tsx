6665b246b34074bf8956c207e90d307cba1d47a4
Alex Galicio <alexisjoygalicio@gmail.com>
2025-10-16 17:58:03 +0800

    feat: make the card same height even if descriptions are different length

--- src/components/landing-page/career-card.tsx
+++ src/components/landing-page/career-card.tsx
@@ -5,9 +5,9 @@ export interface Career {
 
 export default function CareerCard({ title, description }: Career) {
   return (
-    <div className="rounded-lg border p-4 hover:shadow-md">
-      <h2 className="mb-1 font-semibold">{title}</h2>
-      <p className="text-muted-foreground">{description}</p>
+    <div className="h-full rounded-lg border p-4 hover:shadow-md flex flex-col">
+      <h2 className="mb-2 font-semibold">{title}</h2>
+      <p className="text-muted-foreground flex-grow">{description}</p>
     </div>
   );
 }
