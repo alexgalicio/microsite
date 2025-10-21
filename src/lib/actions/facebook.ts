2d148347973e6a0a9b16442be8224e8b96dff288
Alex Galicio <alexisjoygalicio@gmail.com>
2025-10-21 10:07:48 +0800

    feat: add site validation before saving fb credentials

--- src/lib/actions/facebook.ts
+++ src/lib/actions/facebook.ts
@@ -1,13 +1,41 @@
 "use server";
 
 import { createServerSupabaseClient } from "@/utils/server";
+import { auth } from "@clerk/nextjs/server";
 
+// handles saving form
 export async function submitForm(formData: {
   page_id: string;
   access_token: string;
   user_id: string;
 }) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return { success: false, error: "User not signed in" };
+  }
+
   const supabase = createServerSupabaseClient();
+
+  // get users site
+  const { data: siteData, error: siteError } = await supabase
+    .from("sites")
+    .select("id, status")
+    .eq("user_id", userId)
+    .single();
+
+  if (siteError || !siteData) {
+    return {
+      success: false,
+      error: "Please create a site before adding this.",
+    };
+  } else if (siteData.status != "published") {
+    return {
+      success: false,
+      error: "Please make your site public before adding this.",
+    };
+  }
+
   const { error } = await supabase
     .from("facebook_feed")
     .upsert(formData, { onConflict: "user_id" })
@@ -21,6 +49,7 @@ export async function submitForm(formData: {
   return { success: true };
 }
 
+// delete credentials from db
 export async function deleteFacebookCreds(userId: string) {
   const supabase = createServerSupabaseClient();
   const { error } = await supabase
@@ -35,6 +64,7 @@ export async function deleteFacebookCreds(userId: string) {
   return { success: true };
 }
 
+// get saved form values
 export async function getFormValues(user_id: string) {
   const supabase = createServerSupabaseClient();
 
@@ -44,8 +74,9 @@ export async function getFormValues(user_id: string) {
     .eq("user_id", user_id)
     .single();
 
+  // if theres error other than no rows found (PGRST116) return null
   if (error && error.code !== "PGRST116") {
-    throw error;
+    return null;
   }
 
   return data || null;
